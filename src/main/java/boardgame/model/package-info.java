936b3c9bc78c57c75cd7e15a58069429a3492cb5
Balogh András <bandris4@mailbox.unideb.hu>
2025-06-17 16:48:08 +0200

    docs: add package-info.java with JavaDoc for boardgame.model

---
+++ src/main/java/boardgame/model/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Implements the state representation for the Two-Figure Board Puzzle.
+ *
+ * The puzzle consists of an 8x8 board with two figures starting in the top-left corner.
+ * Both figures move simultaneously each turn: one moves vertically, the other horizontally,
+ * by the exact number of cells indicated by their current positions on the board.
+ * The goal is to move both figures together to the bottom-right target cell following the rules.
+ */
+package boardgame.model;
