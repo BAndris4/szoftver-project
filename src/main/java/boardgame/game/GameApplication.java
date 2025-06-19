264b3d46a690edc975009586114e85f0306cb53c
Balogh András <bandris4@mailbox.unideb.hu>
2025-06-19 13:51:15 +0200

    feat: add JavaFX main window with game board and move counter

---
+++ src/main/java/boardgame/game/GameApplication.java
@@ -0,0 +1,21 @@
+package boardgame.game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/game.fxml"));
+        stage.setTitle("JavaFX Board Game");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+}
